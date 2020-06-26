ca0868f212e4a231acd1abf634017fc204e87a88
Gerardo Vargas <evc414@gmail.com>
2020-06-25 22:26:18 -0500

    RN Navigation added

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import 'react-native-gesture-handler';
 
 import React from 'react';
 import {
@@ -13,12 +7,32 @@ import {
   Text,
 } from 'react-native';
 
+import Inicio from './views/Inicio'
+import Nosotros from './views/Nosotros'
+
+//RN
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
 const App = () => {
   return (
     <>
-      <View>
-
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Inicio"
+        >
+          <Stack.Screen 
+              name='Inicio'
+              component={Inicio}
+          />
+          <Stack.Screen 
+              name='Nosotros'
+              component={Nosotros}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
